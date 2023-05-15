ea33f0d5d2de01cb5ce6f7b336192871e969cb83
leduananh96 <leduananh96@gmail.com>
2023-05-15 18:55:32 +0700

    update service port

--- config/constants.js
+++ config/constants.js
@@ -2,7 +2,7 @@ module.exports = {
     SERVICE_CONFIG: {
         TOKEN_EXPIRED_TIME: process.env.TOKEN_EXPIRED_TIME || 60 * 60,
         SECRET_KEY: process.env.JWT_KEY || 'xzcvzxczxzxczasdasd',
-        SERVICE_PORT: process.env.SERVICE_PORT || 3000
+        SERVICE_PORT: process.env.PORT || 3000
     },
 
     DB_CONFIG: {
